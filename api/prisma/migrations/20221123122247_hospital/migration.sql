82ceb4f5112d6db99773df66a770684a5c50f280
José Carlos Oliveira de Lima <carlos.o@academico.ifrn.edu.br>
2022-11-23 09:27:49 -0300

    First Commit

--- api/prisma/migrations/20221123122247_hospital/migration.sql
+++ api/prisma/migrations/20221123122247_hospital/migration.sql
@@ -18,12 +18,12 @@ CREATE TABLE "medico" (
     "telefone" TEXT NOT NULL,
     "nomeCompleto" TEXT NOT NULL,
     "dataDeNascimento" DATETIME NOT NULL,
-    "medicoEspecialidadeId" INTEGER NOT NULL,
-    CONSTRAINT "medico_medicoEspecialidadeId_fkey" FOREIGN KEY ("medicoEspecialidadeId") REFERENCES "MedicoEspecialidade" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "especialidadeId" INTEGER NOT NULL,
+    CONSTRAINT "medico_especialidadeId_fkey" FOREIGN KEY ("especialidadeId") REFERENCES "Especialidade" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
-CREATE TABLE "MedicoEspecialidade" (
+CREATE TABLE "Especialidade" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "especialidade" TEXT NOT NULL
 );
@@ -41,14 +41,27 @@ CREATE TABLE "HorariosAtendimento" (
 );
 
 -- CreateTable
+CREATE TABLE "Calendario" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "diaMensal" INTEGER NOT NULL,
+    "mesAnual" INTEGER NOT NULL,
+    "diasAtendimentoId" INTEGER NOT NULL,
+    "medicoCrm" TEXT NOT NULL,
+    CONSTRAINT "Calendario_diasAtendimentoId_fkey" FOREIGN KEY ("diasAtendimentoId") REFERENCES "DiasAtendimento" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Calendario_medicoCrm_fkey" FOREIGN KEY ("medicoCrm") REFERENCES "medico" ("crm") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
 CREATE TABLE "consulta" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "queixa" TEXT NOT NULL,
     "descricaoMedica" TEXT NOT NULL,
     "prescricaoMedica" TEXT NOT NULL,
     "dataCriado" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "calendarioId" INTEGER NOT NULL,
     "medicoCrm" TEXT NOT NULL,
     "pacienteCpf" TEXT NOT NULL,
+    CONSTRAINT "consulta_calendarioId_fkey" FOREIGN KEY ("calendarioId") REFERENCES "Calendario" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "consulta_medicoCrm_fkey" FOREIGN KEY ("medicoCrm") REFERENCES "medico" ("crm") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "consulta_pacienteCpf_fkey" FOREIGN KEY ("pacienteCpf") REFERENCES "paciente" ("cpf") ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -69,14 +82,13 @@ CREATE TABLE "_HorariosAtendimentoToMedico" (
     CONSTRAINT "_HorariosAtendimentoToMedico_B_fkey" FOREIGN KEY ("B") REFERENCES "medico" ("crm") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "paciente_email_key" ON "paciente"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "paciente_telefone_key" ON "paciente"("telefone");
-
--- CreateIndex
-CREATE UNIQUE INDEX "medico_telefone_key" ON "medico"("telefone");
+-- CreateTable
+CREATE TABLE "_CalendarioToHorariosAtendimento" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+    CONSTRAINT "_CalendarioToHorariosAtendimento_A_fkey" FOREIGN KEY ("A") REFERENCES "Calendario" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_CalendarioToHorariosAtendimento_B_fkey" FOREIGN KEY ("B") REFERENCES "HorariosAtendimento" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "_DiasAtendimentoToMedico_AB_unique" ON "_DiasAtendimentoToMedico"("A", "B");
@@ -89,3 +101,9 @@ CREATE UNIQUE INDEX "_HorariosAtendimentoToMedico_AB_unique" ON "_HorariosAtendi
 
 -- CreateIndex
 CREATE INDEX "_HorariosAtendimentoToMedico_B_index" ON "_HorariosAtendimentoToMedico"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CalendarioToHorariosAtendimento_AB_unique" ON "_CalendarioToHorariosAtendimento"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CalendarioToHorariosAtendimento_B_index" ON "_CalendarioToHorariosAtendimento"("B");
